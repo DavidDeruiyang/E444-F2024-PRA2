a3f13c1a86f73874b1239a980f13d79e5a317c1e
dav <derui.yang@mail.utoronto.ca>
2024-09-25 18:12:02 -0400

    add base.html, move nav bar to base, add form into index page

--- hello.py
+++ hello.py
@@ -10,14 +10,28 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html', current_time=datetime.now())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time=datetime.now(), form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
